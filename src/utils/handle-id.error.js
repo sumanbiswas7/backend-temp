e4c5d3fb14593fbdc50ccc3e104960acadd61deb
sumanbiswas7 <ronaldosuman97@gmail.com>
2023-11-17 14:59:57 +0530

    update: add docs for validate-id func

--- src/utils/handle-id.error.js
+++ src/utils/handle-id.error.js
@@ -1,6 +1,18 @@
 const { HttpError } = require("../helpers/http-error");
 const { HTTP_STATUS } = require("../helpers/http-status");
 
+/**
+ * Validates and handles errors for a given ID.
+ *
+ * @param {string | undefined} id - The ID to validate.
+ * @param {Response} res - Express response object.
+ * @returns {void} - Returns early with a JSON error response if the ID is invalid.
+ *
+ * @example
+ * // Usage in a route handler
+ *    const id = req.params.id;
+ *    validateIDError(id, res);
+ */
 function validateIDError(id, res) {
   const error = new HttpError({});
   const stringId = (id || "").toString();
