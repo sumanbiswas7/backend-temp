302b3850323ba88161ce94c04d6e895de0330f90
sumanbiswas7 <ronaldosuman97@gmail.com>
2023-11-21 15:56:46 +0530

    update: add sheet schemas

--- src/controllers/business.controller.js
+++ src/controllers/business.controller.js
@@ -1,11 +1,14 @@
 const { HttpError } = require("../helpers/http-error");
 const { HTTP_STATUS } = require("../helpers/http-status");
-const Business = require("../models/business.model");
+const { Business, Sheet } = require("../models/business.model");
 const { isValidMongoId } = require("../utils/handle-id.error");
 
 async function get_business(req, res) {
   const error = new HttpError({});
   const id = req.params.id;
+
+  // dummy wait 2 sec
+  // await new Promise((res) => setTimeout(() => res(), 2000));
 
   const idErr = isValidMongoId(id);
   if (idErr.valid === false) {
@@ -36,11 +39,12 @@ async function get_business(req, res) {
  *   }
  */
 async function update_business(req, res) {
+  const error = new HttpError({});
   const id = req.params.id;
 
   const idErr = isValidMongoId(id);
   if (idErr.valid === false) {
-    error.message = idErr.msg;
+    error.message = idErr?.msg;
     error.status = HTTP_STATUS.BAD_REQUEST;
     return res.status(error.status).json(error);
   }
@@ -49,4 +53,38 @@ async function update_business(req, res) {
   res.json(result);
 }
 
-module.exports = { get_business, update_business };
+async function update_business_sheet(req, res) {
+  const error = new HttpError({});
+  const id = req.params.id;
+
+  const idErr = isValidMongoId(id);
+  if (idErr.valid === false) {
+    error.message = idErr?.msg;
+    error.status = HTTP_STATUS.BAD_REQUEST;
+    return res.status(error.status).json(error);
+  }
+
+  const result = await Sheet.updateOne({ _id: id }, req.body);
+  res.json(result);
+}
+
+async function create_buisness(req, res) {
+  const { name } = req.body;
+  const error = new HttpError({});
+
+  if (!name) {
+    error.message = `Business Name is required got ${name}`;
+    error.status = HTTP_STATUS.BAD_REQUEST;
+    return res.status(error.status).json(error);
+  }
+
+  const result = await Business.create({ name });
+  return res.json(result);
+}
+
+module.exports = {
+  get_business,
+  update_business,
+  create_buisness,
+  update_business_sheet,
+};
--- src/routes/business.routes.js
+++ src/routes/business.routes.js
@@ -1,8 +1,13 @@
 const { Router } = require("express");
 const route = Router();
 
-const { get_business } = require("../controllers/business.controller");
+const {
+  get_business,
+  create_buisness,
+  update_business_sheet,
+} = require("../controllers/business.controller");
 const { update_business } = require("../controllers/business.controller");
+const { add_field } = require("../controllers/sheet.controller");
 
 /**
  * --------------------------------------
@@ -11,5 +16,8 @@ const { update_business } = require("../controllers/business.controller");
  */
 route.get("/:id", get_business);
 route.put("/:id", update_business);
+route.put("/sheet/:id", update_business_sheet);
+route.post("/create", create_buisness);
+route.put("/add-field/:id", add_field);
 
 module.exports = route;
